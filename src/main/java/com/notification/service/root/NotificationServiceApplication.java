b9e0a52d89958636dd027886a15113faf848bef0
Basnucaev <96333647+Basnucaev@users.noreply.github.com>
2022-04-30 14:25:52 +0300

    config

--- src/main/java/com/notification/service/root/NotificationServiceApplication.java
+++ src/main/java/com/notification/service/root/NotificationServiceApplication.java
@@ -1,5 +1,7 @@
 package com.notification.service.root;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,8 +10,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@OpenAPIDefinition(info = @Info(title = "Notification Service", version = "1.0", description =
+        "The service is needed in order to create mailings, add clients to the\n" +
+                "    database and launch mailings with a message for suitable clients. Clients\n" +
+                "    for mailing are selected by the code of the mobile operator. You can view\n" +
+                "    the statistics of sent messages for each individual mailing list or for all\n" +
+                "    mailings at once."))
 public class NotificationServiceApplication {
-
 
     public static void main(String[] args) {
 
